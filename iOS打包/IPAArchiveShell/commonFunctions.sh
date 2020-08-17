518b1917c98043a8ead07beab0be746e88b103fd
chenhao <1419707446@qq.com>
2020-08-17 17:56:54 +0800

    脚本添加彩色文字

---
+++ iOS打包/IPAArchiveShell/commonFunctions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#💙💚❤️🧡
+#函数 输出绿色💚正常字符串
+function echoGreen (){
+    if [ $? -eq 0 ];then
+        echo -e "\033[32m 💚 $1 \033[0m"
+    else
+        echo "$1 is no."
+    fi
+}
+
+#函数 输出红色❤️异常字符串
+function echoRed (){
+    if [ $? -eq 0 ];then
+        echo -e "\033[32m ❤️\r $1 \033[0m"
+    else
+        echo "$1 is no."
+    fi
+}
